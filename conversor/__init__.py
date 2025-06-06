022fcd0c91cc71fec0df22e73f1bd226cfb5b4b6
Alonso <laraordonezalonso.com>
2025-06-06 20:01:46 +0200

    Adding __init__.py

---
+++ conversor/__init__.py
@@ -0,0 +1,6 @@
+from .velocity import Velocity
+from .mass import Mass
+from .distance import Distance
+from .temperature import Temperature
+
+__all__ = ["Velocity", "Mass", "Distance", "Temperature"]
